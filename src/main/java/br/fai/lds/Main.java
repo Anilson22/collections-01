752a8c990c96d8cb8b6624b8c3bf01cfc21f2e5b
Anilson22 <pcassioferreira@gmail.com>
2022-03-18 03:38:48 -0300

    Primeiro comit Map concluido

---
+++ src/main/java/br/fai/lds/Main.java
@@ -0,0 +1,36 @@
+package br.fai.lds;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main App = new Main();
+		App.start();
+
+	}
+	private static void start() {
+		Map<String, String> map;
+		map = new HashMap<String, String>();
+		
+		map.put("SRS", "Santa Rita do Sapucai");
+		map.put("PA", "Pouso Alegre");
+		map.put("ITA", "Itajuba");
+		map.put("ZORO", "Conceição dos Ouros");
+		map.put("CAXU", "Cachoeira de Minas");
+		
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+            String chave = entry.getKey();
+            String valor = entry.getValue();
+            System.out.println(String.format("chave: %s | valor: %s", chave, valor));
+        }
+		
+		
+	}
+	
+	private static void print(Map<String, String> item) {
+		
+		
+	}
+}
